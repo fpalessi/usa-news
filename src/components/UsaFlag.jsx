df60fcf0826ed4e858652be609ed22a904f28600
fpalessi <palessi@outlook.es>
2022-09-19 16:01:16 +0200

    second commit

--- src/components/UsaFlag.jsx
+++ src/components/UsaFlag.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import flag from "../assets/usnews.png";
 
 const UsaFlag = () => {
-  return <img src="./src/usnews.png" alt="USA FLAG" height={90} />;
+  return <img src={flag} alt="USA FLAG" height={90} />;
 };
 
 export default UsaFlag;
